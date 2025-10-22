4a61bde27bd32c42e8693601d34ec61f78f215d8
Ahri-Spica <116871488+Ahri-Spica@users.noreply.github.com>
2025-10-22 12:32:03 +0200

    Catalog Service 001
    stubs and skeleton

---
+++ src/Microservice.Catalog.Service/Entity/CatalogItem.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microservice.Catalog.Service.Entity;
+
+public record class CatalogItem
+{
+    public required int     Id           { get; set; }
+    public required string  Name         { get; set; }
+    public required string  Description  { get; set; }
+    public required decimal Price        { get; set; }
+    public required string  PictureUri   { get; set; }
+    public required int     TypeId       { get; set; }
+    public required int     BrandId      { get; set; }
+}
+
+public record class CatalogItemDTO
+{
+    public int     Id           { get; set; }
+    public string  Name         { get; set; }
+    public string  Description  { get; set; }
+    public decimal Price        { get; set; }
+    public string  PictureUri   { get; set; }
+    public int     TypeId       { get; set; }
+    public int     BrandId      { get; set; }
+    public string  TypeName     { get; set; }
+    public string  BrandName    { get; set; }
+    public bool    IsValidMatch { get; set; } = true;
+
+    public CatalogItemDTO(CatalogItem item, CatalogBrand brand, CatalogType type) 
+    {
+        if ((item.BrandId != brand.Id) || (item.TypeId != type.Id))
+        {
+            this.IsValidMatch = false;
+        }
+        
+        this.Id           = item.Id;
+        this.Name         = item.Name;
+        this.Description  = item.Description;
+        this.Price        = item.Price;
+        this.PictureUri   = item.PictureUri;
+        this.TypeId       = item.TypeId;
+        this.BrandId      = item.BrandId;
+        this.TypeName     = type.Type;
+        this.BrandName    = brand.Brand;
+    }
+}
---
+++ src/Microservice.Catalog.Service/Interfaces/ICatalogItemRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microservice.Catalog.Service.Entity;
+
+namespace Microservice.Catalog.Service.Interfaces;
+
+public interface ICatalogItemRepo //temp
+{
+    Task<CatalogItemDTO?> GetAsync(int  id);
+    Task<IReadOnlyCollection<CatalogItemDTO>> ListAsync();
+    Task<CatalogItem> AddAsync(CatalogItem item);
+    Task<CatalogItem> UpdateAsync(CatalogItem item);
+    Task<bool> DeleteAsync(int id);
+}
